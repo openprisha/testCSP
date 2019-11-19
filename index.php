b5fe0b4c872b55048e7310ace93117002628f888
prishaparashar <parashar.prisha@gmail.com>
2019-11-19 16:26:39 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+    
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links Prisha Parashar, Period 1</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Parashar_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Parashar_Projects.html">Projects.html</a></li></br>
+      <li><a href="Parashar_Resume.html">Resume.html</a></li></br>
+      <li><a href="Parashar_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+
+    
+  </body>
+</html>
